d3021eb376e346bfd51d9433ee9192327b31c1b5
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2021-03-02 12:45:10 +0800

    Building our React app

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 const config = {
+  STRIPE_KEY:
+    "pk_test_51INbVXIX0KbRZO0kYqJDAKUHHzXobXknTQddq09UhaxdNZHMlnTk38LPms4ZgjJbmfXFktRg7CDcVqhHlJflOAHH00VmEATtUg",
   s3: {
     REGION: "us-east-1",
     BUCKET: "notes-api-prod-serverlessdeploymentbucket-1jzuy884mv1a",
@@ -11,7 +13,7 @@ const config = {
     REGION: "us-east-1",
     USER_POOL_ID: "us-east-1_xk9MDdydg",
     APP_CLIENT_ID: "1pk5mrt2e5npbskdaslnka8g8f",
-    IDENTITY_POOL_ID: "us-east-1:5a4c286d-cc4a-465a-942b-992e2b9f0796",
+    IDENTITY_POOL_ID: "us-east-1:870a3118-a995-454b-b367-0dc481aac417",
   },
 };
 
