ce78c02042b136d5a077cff847d8a38f8117ae44
Erdenebulgan Sainkhishig <erdenebulgandeveloper@gmail.com>
2021-03-19 18:17:38 +0800

    change home.js

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -91,6 +91,9 @@ export default function Home() {
   return (
     <div className="Home">
       {isAuthenticated ? renderNotes() : renderLander()}
+
+      {/* {isAuthenticated ? renderNotes() : renderLander()}
+      {isAuthenticated.none.no} */}
     </div>
   );
 }
